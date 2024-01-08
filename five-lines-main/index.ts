d7f28c59d8f83cfa8a8e3efd71b83c88f30e9c6c
dongjuppp <dongjuppp@gmail.com>
2024-01-08 23:29:13 +0900

    4장1/2

--- five-lines-main/index.ts
+++ five-lines-main/index.ts
@@ -14,8 +14,103 @@ enum Tile {
   KEY2, LOCK2
 }
 
-enum Input {
-  UP, DOWN, LEFT, RIGHT
+interface Input {
+  isRight(): boolean;
+  isLeft(): boolean;
+  isUp(): boolean;
+  isDown(): boolean;
+  handle(): void;
+}
+
+class Right implements Input {
+  isDown(): boolean {
+    return false;
+  }
+
+  isLeft(): boolean {
+    return false;
+  }
+
+  isRight(): boolean {
+    return true;
+  }
+
+  isUp(): boolean {
+    return false;
+  }
+
+  handle() {
+    moveHorizontal(1);
+  }
+
+}
+
+class Left implements Input {
+  isDown(): boolean {
+    return false;
+  }
+
+  isLeft(): boolean {
+    return true;
+  }
+
+  isRight(): boolean {
+    return false;
+  }
+
+  isUp(): boolean {
+    return false;
+  }
+
+  handle() {
+    moveHorizontal(-1);
+  }
+
+}
+
+class Up implements Input {
+  isDown(): boolean {
+    return false;
+  }
+
+  isLeft(): boolean {
+    return false;
+  }
+
+  isRight(): boolean {
+    return false;
+  }
+
+  isUp(): boolean {
+    return true;
+  }
+
+  handle() {
+    moveVertical(-1);
+  }
+}
+
+class Down implements Input {
+  isDown(): boolean {
+    return true;
+  }
+
+  isLeft(): boolean {
+    return false;
+  }
+
+  isRight(): boolean {
+    return false;
+  }
+
+  isUp(): boolean {
+    return false;
+  }
+
+  handle() {
+    moveVertical(1);
+  }
+
 }
 
 let playerx = 1;
@@ -50,12 +145,12 @@ function moveToTile(newx: number, newy: number) {
 
 function moveHorizontal(dx: number) {
   if (map[playery][playerx + dx] === Tile.FLUX
-    || map[playery][playerx + dx] === Tile.AIR) {
+      || map[playery][playerx + dx] === Tile.AIR) {
     moveToTile(playerx + dx, playery);
   } else if ((map[playery][playerx + dx] === Tile.STONE
-    || map[playery][playerx + dx] === Tile.BOX)
-    && map[playery][playerx + dx + dx] === Tile.AIR
-    && map[playery + 1][playerx + dx] !== Tile.AIR) {
+          || map[playery][playerx + dx] === Tile.BOX)
+      && map[playery][playerx + dx + dx] === Tile.AIR
+      && map[playery + 1][playerx + dx] !== Tile.AIR) {
     map[playery][playerx + dx + dx] = map[playery][playerx + dx];
     moveToTile(playerx + dx, playery);
   } else if (map[playery][playerx + dx] === Tile.KEY1) {
@@ -69,7 +164,7 @@ function moveHorizontal(dx: number) {
 
 function moveVertical(dy: number) {
   if (map[playery + dy][playerx] === Tile.FLUX
-    || map[playery + dy][playerx] === Tile.AIR) {
+      || map[playery + dy][playerx] === Tile.AIR) {
     moveToTile(playerx, playery + dy);
   } else if (map[playery + dy][playerx] === Tile.KEY1) {
     remove(Tile.LOCK1);
@@ -88,19 +183,8 @@ function update() {
 function handleInputs() {
   while (inputs.length > 0) {
     let current = inputs.pop();
-    handelInput(current);
+    current.handle();
   }
-}
-
-function handelInput(input: Input) {
-  if (input === Input.LEFT)
-    moveHorizontal(-1);
-  else if (input === Input.RIGHT)
-    moveHorizontal(1);
-  else if (input === Input.UP)
-    moveVertical(-1);
-  else if (input === Input.DOWN)
-    moveVertical(1);
 }
 
 function updateMap() {
@@ -141,32 +225,32 @@ function createGraphics() {
 
   return g
 }
-  function drawMap(g: CanvasRenderingContext2D) {
-    for (let y = 0; y < map.length; y++) {
-      for (let x = 0; x < map[y].length; x++) {
-        if (map[y][x] === Tile.FLUX)
-          g.fillStyle = "#ccffcc";
-        else if (map[y][x] === Tile.UNBREAKABLE)
-          g.fillStyle = "#999999";
-        else if (map[y][x] === Tile.STONE || map[y][x] === Tile.FALLING_STONE)
-          g.fillStyle = "#0000cc";
-        else if (map[y][x] === Tile.BOX || map[y][x] === Tile.FALLING_BOX)
-          g.fillStyle = "#8b4513";
-        else if (map[y][x] === Tile.KEY1 || map[y][x] === Tile.LOCK1)
-          g.fillStyle = "#ffcc00";
-        else if (map[y][x] === Tile.KEY2 || map[y][x] === Tile.LOCK2)
-          g.fillStyle = "#00ccff";
+function drawMap(g: CanvasRenderingContext2D) {
+  for (let y = 0; y < map.length; y++) {
+    for (let x = 0; x < map[y].length; x++) {
+      if (map[y][x] === Tile.FLUX)
+        g.fillStyle = "#ccffcc";
+      else if (map[y][x] === Tile.UNBREAKABLE)
+        g.fillStyle = "#999999";
+      else if (map[y][x] === Tile.STONE || map[y][x] === Tile.FALLING_STONE)
+        g.fillStyle = "#0000cc";
+      else if (map[y][x] === Tile.BOX || map[y][x] === Tile.FALLING_BOX)
+        g.fillStyle = "#8b4513";
+      else if (map[y][x] === Tile.KEY1 || map[y][x] === Tile.LOCK1)
+        g.fillStyle = "#ffcc00";
+      else if (map[y][x] === Tile.KEY2 || map[y][x] === Tile.LOCK2)
+        g.fillStyle = "#00ccff";
 
-        if (map[y][x] !== Tile.AIR && map[y][x] !== Tile.PLAYER)
-          g.fillRect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
-      }
+      if (map[y][x] !== Tile.AIR && map[y][x] !== Tile.PLAYER)
+        g.fillRect(x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
     }
   }
+}
 
-  function drawPlayer(g: CanvasRenderingContext2D) {
-    g.fillStyle = "#ff0000";
-    g.fillRect(playerx * TILE_SIZE, playery * TILE_SIZE, TILE_SIZE, TILE_SIZE);
-  }
+function drawPlayer(g: CanvasRenderingContext2D) {
+  g.fillStyle = "#ff0000";
+  g.fillRect(playerx * TILE_SIZE, playery * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+}
 
 function gameLoop() {
   let before = Date.now();
@@ -187,9 +271,9 @@ const UP_KEY = "ArrowUp";
 const RIGHT_KEY = "ArrowRight";
 const DOWN_KEY = "ArrowDown";
 window.addEventListener("keydown", e => {
-  if (e.key === LEFT_KEY || e.key === "a") inputs.push(Input.LEFT);
-  else if (e.key === UP_KEY || e.key === "w") inputs.push(Input.UP);
-  else if (e.key === RIGHT_KEY || e.key === "d") inputs.push(Input.RIGHT);
-  else if (e.key === DOWN_KEY || e.key === "s") inputs.push(Input.DOWN);
+  if (e.key === LEFT_KEY || e.key === "a") inputs.push(new Left());
+  else if (e.key === UP_KEY || e.key === "w") inputs.push(new Up());
+  else if (e.key === RIGHT_KEY || e.key === "d") inputs.push(new Right());
+  else if (e.key === DOWN_KEY || e.key === "s") inputs.push(new Down());
 });
 
